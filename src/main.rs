1c7e853c30c9f00b0c6e271fc7107aa7c11fba12
Francisco Casiano <34542704+panchi64@users.noreply.github.com>
2024-01-03 14:16:05 -0400

    Added TODOs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
     println!("Hello, world!");
 }
+
+// TODO: [ ] Create the webserver
+// TODO: [ ] Run system checks
+// TODO: [ ] Create the UI for the website to be projected
+// TODO: [ ] Create the UI for the website students will see
+// TODO: [ ] Create .csv file for use with Excel
+// TODO: [ ] Add moodle API support
+// TODO: [ ] Add student's name gathering automation through Moodle API
+// TODO: [ ] Create automated github workflow for building OS specific packages and adding them to a release page
+// TODO: [ ] Make a custom icon to show on the OS when the application is built
+// TODO: [ ] Error handling for when a user's device has already been used to mark attendance
+// TODO: [ ] Implement a way to retain a list of user devices that have already marked a student as attending. Clear the list on application exit and application start
