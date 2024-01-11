8682055055963cff6e400658227a8dac84efe6f8
Francisco Casiano <personal@franciscocasiano.com>
2024-01-11 16:17:45 -0400

    Added simple layout for the middle part of the page and "News/Comments" component.

---
+++ src/ui/frontend/src/components/NewsandComments.tsx
@@ -0,0 +1,13 @@
+function NewsandComments() {
+    return(
+        <div class="w-full h-full grid-rows-8">
+            <div class="row-span-1 row-start-1 text-3xl m-2">
+                News / Comments
+                <div class="divider my-2"></div>
+            </div>
+            <textarea class="textarea-block bg-transparent px-4 pt-1 pb-4 w-full border-0" placeholder="Today's notes..."></textarea>
+        </div>
+    );
+}
+
+export default NewsandComments;
--- src/ui/frontend/src/routes/index.tsx
+++ src/ui/frontend/src/routes/index.tsx
@@ -1,31 +1,42 @@
-import { A } from "@solidjs/router";
+import {A} from "@solidjs/router";
 import LogoComponent from "../components/Logo";
 import OfficeHoursComponent from "../components/OfficeHours";
 import CourseDetailsComponent from "../components/CourseDetails";
+import NewsandCommentsComponents from "../components/NewsandComments"
 
 export default function Home() {
-  return (
-    <main>
-      <div class="h-[25vh] w-full border-amber-200 border-2">
-        <div class="grid grid-cols-2 h-full place-items-center gap-8">
-          {/*Leftmost Header*/}
-          <div class="w-full border-purple-500 border-2 p-4">
-            <div class="justify-start flex flex-row place-items-center">
-              <LogoComponent logoPath="/UPRM-logo.png" universityName=""/>
-              <OfficeHoursComponent days="LMV" timePeriod="10am-12pm"/>
+    return (
+        <main>
+            <div class="h-[25vh] w-full border-amber-200 border-2">
+                <div class="grid grid-cols-2 h-full place-items-center gap-8">
+                    {/*Leftmost Header*/}
+                    <div class="w-full border-purple-500 border-2 p-4">
+                        <div class="justify-start flex flex-row place-items-center">
+                            <LogoComponent logoPath="/UPRM-logo.png" universityName=""/>
+                            <OfficeHoursComponent days="LMV" timePeriod="10am-12pm"/>
+                        </div>
+                    </div>
+                    {/*Rightmost Header*/}
+                    <div class="w-full border-green-500 border-2 place-content-center p-4">
+                        <CourseDetailsComponent courseName="INEL4025" sectionNumbers={['100', '096', '060', '042']}
+                                                professorName="goomba steinhold"/>
+                    </div>
+                </div>
             </div>
-          </div>
-          {/*Rightmost Header*/}
-          <div class="w-full border-green-500 border-2 place-content-center p-4">
-            <CourseDetailsComponent courseName="INEL4025" sectionNumbers={['100', '096', '060', '042']}
-                                    professorName="goomba steinhold"/>
-          </div>
-        </div>
-      </div>
-      {/*Body*/}
-      <div class="h-[65vh] w-full border-blue-500 border-2"></div>
-      {/*Footer*/}
-      <div class="h-[10vh] w-full border-red-500 border-2"></div>
-    </main>
-  );
+            {/*Body*/}
+            <div class="h-[65vh] w-full border-blue-500 border-2 grid grid-cols-3">
+                <div class="col-span-2 col-start-1 flex flex-col">
+                    <div>{/*<AttendanceCounterComponent/>*/}</div>
+                    <div><NewsandCommentsComponents/></div>
+                </div>
+                <div class="col-span-1 col-start-3">
+                    {/*<QRCodeComponent/>*/}
+                    {/*<ConfirmationCodeComponent/>*/}
+                </div>
+            </div>
+
+            {/*Footer*/}
+            <div class="h-[10vh] w-full border-red-500 border-2"></div>
+        </main>
+    );
 }
