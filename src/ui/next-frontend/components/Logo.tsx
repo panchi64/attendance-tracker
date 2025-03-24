e77c3b0d1378853c79bffa31f3c9f6f152fc3143
Francisco Casiano <34542704+panchi64@users.noreply.github.com>
2025-03-23 20:23:09 -0400

    Nuked earth :D

--- src/ui/next-frontend/components/Logo.tsx
+++
@@ -1,9 +0,0 @@
-function Logo(props: { logoPath: string, universityName: string}) {
-  return (
-    <div className="p-2 h-48 w-48 mr-8">
-      <img src={props.logoPath} alt={props.universityName}/>
-    </div>
-  )
-}
-
-export default Logo;
